0177396302c7648a44c5bb54d2bd22aa52caf0ea
marubontan <marubontan@gmail.com>
2018-04-20 15:36:48 -0300

    mv info

--- example.jl
+++
@@ -1,9 +0,0 @@
-include("./src/em.jl")
-using Distributions
-
-groupOne = rand(MvNormal([1,1], eye(2)), 100)
-groupTwo = rand(MvNormal([10,10], eye(2)), 100)
-
-data = hcat(groupOne, groupTwo)'
-
-println(EM(data, 2))

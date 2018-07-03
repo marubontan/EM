845f0359d52a6f7d048f75d2be1b19754e61066d
marubontan <marubontan@gmail.com>
2018-07-03 17:26:49 -0300

    refine examples

--- example/example.jl
+++ example/example.jl
@@ -7,4 +7,12 @@ groupThree = rand(MvNormal([100,100], eye(2)), 100)
 
 data = hcat(groupOne, groupTwo, groupThree)'
 
-println(EM(data, 3))
+emResults = EM(data, 3)
+println("mean: [1, 1],[10, 10], [100, 100]")
+@show emResults.mu[end]
+println("sigma: [1.0 0.0; 0.0 1.0], [1.0 0.0; 0.0 1.0], [1.0 0.0; 0.0 1.0]")
+@show emResults.sigma[end]
+println("mix: [0.3, 0.3, 0.3]")
+@show emResults.mix[end]
+@show emResults.logLikelihoods[end]
+@show emResults.iterCount
